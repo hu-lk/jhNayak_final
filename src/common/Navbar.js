d7afb67d86851a3d848436298370315c0a94b670
hu-lk <56403979+hu-lk@users.noreply.github.com>
2023-08-18 14:14:56 +0530

    second

--- src/common/Navbar.js
+++ src/common/Navbar.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom'; // Assuming you're using React Router for navigation
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav>
+      <ul>
+        <ul><Link to="/">Home</Link></ul>
+        <ul><Link to="/about">About</Link></ul>
+        <ul><Link to="/programs">Programs</Link></ul>
+        <ul><Link to="/contacts">Contacts</Link></ul>
+      </ul>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
